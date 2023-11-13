d4ebc75a9779051e1524949801d82d82b9993e80
Pushkar Kurhekar <pshkrh@gmail.com>
2023-11-13 18:20:47 -0500

    Implement backend for A5

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
+
 app.listen(4000);

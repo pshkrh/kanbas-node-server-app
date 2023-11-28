52f56dfa0987bf886f619a859030a6c6feb1dae4
Pushkar Kurhekar <pshkrh@gmail.com>
2023-11-27 19:57:46 -0500

    Use connection string from env

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ import session from "express-session";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 
-mongoose.connect("mongodb://localhost:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
@@ -23,9 +25,15 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
-app.use(
-    session(sessionOptions)
-);
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 
 UserRoutes(app);
